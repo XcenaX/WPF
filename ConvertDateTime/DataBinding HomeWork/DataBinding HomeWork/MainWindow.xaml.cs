d86017eaf90f0f615ef14436172a7330fade04ac
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-02-11 18:11:09 +0600

    Add files via upload

---
+++ ConvertDateTime/DataBinding HomeWork/DataBinding HomeWork/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding_HomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertData(object sender, RoutedEventArgs e)
+        {
+            string date = dateText.Text;
+            try
+            {
+                var convertedDate = Convert.ToDateTime(date);
+                convertedBlock.Text = convertedDate.ToString();
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Некорректный формат!!!");
+            }
+        }
+
+        private void ConvertBack(object sender, RoutedEventArgs e)
+        {
+            string date = dateText.Text;
+
+            var parsedDate = date.Split('.',' ');
+            if(parsedDate.Count() < 3)
+            {
+                MessageBox.Show("Некорректный формат!!!");
+                return;
+            }
+            convertedBlock.Text = parsedDate[0] + " " + parsedDate[1] + " " + parsedDate[2];
+        }
+    }
+}
